08b63790b926a1f158ccc8bbe37fd032c26df369
AleksandrMalinin <alexander.s.malinin@gmail.com>
2017-02-11 20:30:37 +0300

    Декоративные элементы

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var link = document.querySelector(".offer-btn");
+var popup = document.querySelector(".form");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.toggle("form-show");
+});
+
+/*Сделал немного javascript, доделаю позже так как не могу посмотреть задание*/
