c0162874dca8cb8d44c1f44b936950937be67e76
AleksandrMalinin <alexander.s.malinin@gmail.com>
2017-02-15 17:21:16 +0300

    Декоративные исправления-2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 var link = document.querySelector(".offer-btn");
 var popup = document.querySelector(".form");
-
-var form = popup.querySelector(".hotels-form");
+var form = popup.querySelector("form");
 var arrival = popup.querySelector("[name=arrival]");
 var departure = popup.querySelector("[name=departure]");
 var adult = popup.querySelector("[name=adult]");
+var children = popup.querySelector("[name=children]");
+var storageAdult = localStorage.getItem("adult");
+var storageChildren = localStorage.getItem("children");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("form-close");
+  if (storageAdult, storageChildren) {
+    adult.value = storageAdult;
+    children.value = storageChildren;
+  }
 });
 
 form.addEventListener("submit", function(event) {
-   if (!arrival.value || !departure.value || !adult.value);
+  if (!arrival.value || !departure.value || !adult.value || !children.value) {
+    event.preventDefault();
+  } else {
+    localStorage.setItem("adult", adult.value);
+    localStorage.setItem("children", children.value);
+  }
 });
 
 /*Еще не доделал*/
