4e87179acc0f3140f154b687e3a4b5529b53774f
AleksandrMalinin <alexander.s.malinin@gmail.com>
2017-02-15 12:28:45 +0300

    Декоративные исправления

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 var link = document.querySelector(".offer-btn");
 var popup = document.querySelector(".form");
 
+var form = popup.querySelector(".hotels-form");
+var arrival = popup.querySelector("[name=arrival]");
+var departure = popup.querySelector("[name=departure]");
+var adult = popup.querySelector("[name=adult]");
+
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("form-close");
 });
 
-/*Сделал немного javascript, доделаю позже так как не могу посмотреть задание*/
+form.addEventListener("submit", function(event) {
+   if (!arrival.value || !departure.value || !adult.value);
+});
+
+/*Еще не доделал*/
